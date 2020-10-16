e1140b3c1bb9971f35bf2ae3e776c39c6909406b
leonardoaazevedo <72950125+leonardoaazevedo@users.noreply.github.com>
2020-10-15 21:41:34 -0300

    Create Vendedor.hpp

---
+++ Vendedor.hpp
@@ -0,0 +1,17 @@
+
+#include <string>
+#include "Empregado.hpp"
+	
+class Vendedor : public Empregado {
+    
+  private:
+    double quotaMensalVendas;
+
+  public:
+    Vendedor (std::string _nome, double _salarioHora, double _horasTrabalhadas, int _quotaMensalVendas);	
+	  double quotaTotalAnual();
+    void printInfo () override ;
+    ~Vendedor(){};
+};
+
+#endif
