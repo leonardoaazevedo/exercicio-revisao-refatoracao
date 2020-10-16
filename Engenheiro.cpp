04e09719b4dba8366904dfdcd1df20d7e6105c6e
leonardoaazevedo <72950125+leonardoaazevedo@users.noreply.github.com>
2020-10-15 21:37:17 -0300

    Update Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,16 @@
 #include <string>
-#include "Empregado.hpp"
+#include "Engenheiro.hpp"
 
-class Engenheiro : public Empregado {
+ Engenheiro::Engenheiro (std::string _nome, double _salarioHora, double _horasTrabalhadas, int _projetos){
+     this->nome = _nome;
+     this->salarioHora = _salarioHora;
+     this->horasTrabalhadas = _horasTrabalhadas;
+     this->projetos = _projetos;
+   }
 
-  public:
-    std::string nome;  
-	int projetos;
-	
-};
-
+ void Engenheiro::printInfo ()  {
+     std::cout << "Nome: " << this->nome << std::endl;
+     std::cout << "Salario Mes: " << this->pagamentoMes() << std::endl;
+     std::cout << "Projetos: " << this->projetos << std::endl;
+     std::cout << std::endl; 
+ }
